00409ce5478ac4da0703fa9a6dd546c4957d321a
Elias Soong <elias.soong@gmail.com>
2014-09-10 14:40:42 +0800

    从主代码分离配置文件。

--- KSQServer/__init__.py
+++ KSQServer/__init__.py
@@ -5,18 +5,29 @@ import os, os.path
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, static_folder='files')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1:8889/keshaq'
-db = SQLAlchemy(app)
 
-# Create dummy secrey key so we can use flash
-app.config['SECRET_KEY'] = '123456790'
+app = Flask(__name__)
+app.config.from_object('KSQServer.default_settings')
+try:
+    app.config.from_envvar('KSQSERVER_SETTINGS')
+except Exception:
+    pass
+static_folder = app.config['STATIC_FOLDER']
+
+app = Flask(__name__, static_folder=static_folder)
+app.config.from_object('KSQServer.default_settings')
+try:
+    app.config.from_envvar('KSQSERVER_SETTINGS')
+except Exception:
+    pass
+
+db = SQLAlchemy(app)
 
 db.app = app
 db.init_app(app)
 
 # Create directory for file fields to use
-file_path = os.path.join(os.path.dirname(__file__), 'files')
+file_path = os.path.join(os.path.dirname(__file__), static_folder) if not static_folder.startswith('/') else static_folder
 try:
     os.mkdir(file_path)
 except OSError:
---
+++ KSQServer/default_settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# 强制 json 输出采用 utf-8 ：
+JSON_AS_ASCII = False
+# Create dummy secrey key so we can use flash
+SECRET_KEY = 'YouYouMM_and_KeshaQ_SEC_KEY_408'
+# 设置静态文件（主要是图片）存储路径
+STATIC_FOLDER = 'files'
+# 数据库连接设置：
+SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1:8889/keshaq'
+
+
