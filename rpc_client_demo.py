0c30c34b9dbb07ea46d432a02b6cb25c51488eb1
Elias Soong <elias.soong@gmail.com>
2014-09-28 18:36:57 +0800

    更新 api 客户端示例，增加服务器对时处理。

--- rpc_client_demo.py
+++ rpc_client_demo.py
@@ -3,16 +3,32 @@
 import requests
 import time
 import hashlib
+import json
 
 from flask.ext.hmacauth import hmac
 
-path_and_query = "/rpc/accumulator?timestamp="+str(int(time.time()))+"&key=4nM^mLISvh&a=10&b=100"
-host = "http://127.0.0.1:5000"
-sig=hmac.new("Yu8{Lnka%Y", digestmod=hashlib.sha1, msg=path_and_query).hexdigest()
-print sig
-req = requests.get(host+path_and_query, headers={'X-Auth-Signature': sig})
-print req
-print req.headers
-print req.text
+#host = "http://127.0.0.1:5000"
+host = "http://rpc.youyoumm.com"
+
+# 首先进行对时：
+ts = time.time()
+resp = requests.get(host+"/rpc/time")
+print resp
+print resp.text
+server_ts = json.loads(resp.text)['timestamp']
+# 注意：服务器时间戳检查不允许时间戳快于服务器时间，因此做对时调整时需要稍稍多调慢一些，才能增大连接成功的概率！
+time_diff = ts - server_ts
+print 'time diff:', time_diff, '\n'
+
+# 正式调用服务器接口：
+timestamp = int(time.time() - time_diff - 1)
+path_and_query = "/rpc/accumulator?timestamp="+str(timestamp)+"&key=4nM^mLISvh&a=10&b=100"
+hasher = hmac.new("Yu8{Lnka%Y", digestmod=hashlib.sha1, msg=path_and_query)
+# hasher.update(request.body)   # ToDo: 如果是 POST 方法发送的，则 POST 的 body 也需要加入签名内容！
+sig = hasher.hexdigest()
+print 'sig:', sig
+resp = requests.get(host+path_and_query, headers={'X-Auth-Signature': sig})
+print resp
+print resp.text
 
 
