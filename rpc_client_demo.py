c76e4b504bbb2eb5f1cc08d04e3256d7b1357c36
Elias Soong <elias.soong@gmail.com>
2014-10-09 18:02:16 +0800

    修正客户端示例代码和参考配置文件格式。

--- rpc_client_demo.py
+++ rpc_client_demo.py
@@ -21,7 +21,7 @@ time_diff = ts - server_ts
 print 'time diff:', time_diff, '\n'
 
 # 正式调用服务器接口：
-timestamp = int(time.time() - time_diff - 1)
+timestamp = int(time.time() - time_diff - 2)
 path_and_query = "/rpc/accumulator?timestamp="+str(timestamp)+"&key=demo_key&a=10&b=100"
 hasher = hmac.new("demo_secret", digestmod=hashlib.sha1, msg=path_and_query)
 # hasher.update(request.body)   # ToDo: 如果是 POST 方法发送的，则 POST 的 body 也需要加入签名内容！
