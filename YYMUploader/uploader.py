05c6f6776f80da8b2e2351e1b145b503a0c719d1
Elias Soong <elias.soong@gmail.com>
2014-11-08 09:07:58 +0800

    增加新文件监控功能。

--- YYMUploader/uploader.py
+++ YYMUploader/uploader.py
@@ -11,6 +11,8 @@ import hmac
 
 import requests
 import qiniu.io
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 ''' 如果 exe 程序出现杀毒软件报错，可以用 upx 加壳来保护。'''
 
@@ -20,7 +22,10 @@ API_SECRET = 'Nj4_iv_52Y'
 
 LOG_NAME = 'uploader.log'
 
+# 准备全局变量：
 default_encoding = sys.stdin.encoding
+history_dic = {}
+user_id = None
 
 current_path = os.path.split(os.path.realpath(__file__))[0]
 print '=', u'尝试上传目录 %s 中未上传过的图片文件（jpg, png, gif）：' % current_path
@@ -92,7 +97,7 @@ def upload_image(file_path, id, type, user, note, name):
         return (resp, resp)
 
 def walk(root, path):
-    ''' 辅助函数：返回指定目录下所有文件及路径。'''
+    ''' 辅助函数：返回指定目录下所有文件及路径。''' # 其实用 os.path.walk 可以完全代替这个函数的。。
     root_path = os.path.join(root, path)
     entries = []
     for entry in os.listdir(root_path):
@@ -102,59 +107,84 @@ def walk(root, path):
             entries.extend(map(lambda x: (os.path.join(entry, x[0]), x[1]), walk(root, os.path.join(path, entry))))
     return entries
 
-# 用户登陆
-user_id = None
-while not user_id:
-    print '=', u'用户名：',
-    username = raw_input()
-    print '=', u'密码：',
-    password = getpass.getpass('')
-
-    path = '/rpc/tokens'
-    payload = {'username': username,
-               'password': password,
-               'device': 'YYMUploader',
-              }
-    resp = rpc_post(path, {}, payload)
-    status = resp['status']
-    if status == 201:
-        user_id = resp['data']['user_id']
-        print '=', u'成功以用户 id %d 登陆！' % user_id
-    else:
-        print '=', u'用户或密码不正确！'
-
-# 读取历史处理日志：
-history_dic = {}
-with codecs.open(os.path.join(current_path, LOG_NAME), 'r', default_encoding) as f:
-    for line in f.readlines():
-        time_str, level, message = line.split('|', 2)
-        filename, note, id_str, resp = message.strip().split(':', 3)
-        level = level.strip()
-        filename = filename.strip()
-        if level.lower() == 'info':
-            history_dic[filename] = True
-
-# 列出图片文件：
-current_path = current_path.decode(default_encoding)
-for dir, filename in walk(current_path, ''):
+def process_file(dir, filename):
+    ''' 辅助函数：对指定路径图片，完成上传过程。'''
     endfix = filename.split('.')[-1].lower()
     if endfix in ['jpg', 'jpeg', 'png', 'gif']:
         full_path = os.path.join(current_path, dir, filename)
         filename, note = extract(filename)
         key_path = os.path.join(dir, filename)
         if history_dic.has_key(key_path):
-            continue
+            return False
+        print '-', u'发现新图片：', key_path
         ret, err = upload_image(full_path, 0, 2, user_id, note, filename)
         if err is None:
             image_id = ret['data']['id']
             print '*', key_path, u'上传成功。id 为：', image_id, u'注释为：', note
             logger.info(key_path + u':' + note + u':' + unicode(image_id) + u':' + unicode(ret))
+            history_dic[key_path] = True
         else:
             print '*', key_path, u'上传出错！', err
             logger.error(key_path + u':' + note + u': :' + err)
 
-print '=', u'找不到更多未上传的图片文件了！'
-print '=', u'按回车键结束程序运行。'
-raw_input()
+
+class NewFileHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory == False:
+            new_file_path = event.src_path if type(event.src_path == unicode) else unicode(event.src_path, default_encoding)
+            relative_path = os.path.relpath(new_file_path, current_path)
+            dir, filename = os.path.split(relative_path)
+            process_file(dir, filename)
+
+
+if __name__ == "__main__":
+    # 用户登陆
+    while not user_id:
+        print '=', u'用户名：',
+        username = raw_input()
+        print '=', u'密码：',
+        password = getpass.getpass('')
+
+        path = '/rpc/tokens'
+        payload = {'username': username,
+                   'password': password,
+                   'device': 'YYMUploader',
+                  }
+        resp = rpc_post(path, {}, payload)
+        status = resp['status']
+        if status == 201:
+            user_id = resp['data']['user_id']
+            print '=', u'成功以用户 id %d 登陆！' % user_id
+        else:
+            print '=', u'用户或密码不正确！'
+
+    # 读取历史处理日志：
+    with codecs.open(os.path.join(current_path, LOG_NAME), 'r', default_encoding) as f:
+        for line in f.readlines():
+            time_str, level, message = line.split('|', 2)
+            filename, note, id_str, resp = message.strip().split(':', 3)
+            level = level.strip()
+            filename = filename.strip()
+            if level.lower() == 'info':
+                history_dic[filename] = True
+
+    # 列出图片文件：
+    current_path = current_path.decode(default_encoding)
+    for dir, filename in walk(current_path, ''):
+        process_file(dir, filename)
+
+    print '=', u'找不到更多未上传的图片文件了，将监控新放入的文件！'
+    print '=', u'按 Ctrl+C 结束程序运行。'
+
+    event_handler = NewFileHandler()
+    observer = Observer()
+    observer.schedule(event_handler, current_path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 
